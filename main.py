a70211a08c0ab5a7b690ee5807a12bb80c94d6d9
Mitch Malvin <e0773911@u.nus.edu>
2023-06-17 22:07:19 +0800

    fix chain to retrieve only relevant information

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ from langchain.memory import ConversationBufferMemory
 import openai
 
 
-
-
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 loader = TextLoader("data.txt")
@@ -21,21 +19,62 @@ documents = loader.load()
 
 # split the documents, create embeddings for them, 
 # and put them in a vectorstore  to do semantic search over them.
-text_splitter = CharacterTextSplitter(chunk_size=60000, chunk_overlap=0)
+text_splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=30)
 documents = text_splitter.split_documents(documents)
 
-embeddings = OpenAIEmbeddings()
-vectorstore = Chroma.from_documents(documents, embeddings)
+#uncomment this to look at the chunking
+for doc in documents :
+    print(doc)
+    print("\n \n \n \n")
 
-#  create a memory object
-#  to track the inputs/outputs and hold a conversation
+embeddings = OpenAIEmbeddings()
+vectorstore = Chroma.from_documents(documents, embeddings, persist_directory='./data')
+
+vectorstore.persist()
+
+
+#  create a memory object to track the inputs/outputs and hold a conversation
+
+## for ConversationalRetrievalChain, which is just RetrievalQA chain with memory
 conversational_memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
+prompt_template = """Use the following pieces of context to answer the question at the end. Analyze the given conversation in the context and understand the talking style as well as certain slang words that Wei often uses. You are to reply me as Wei and only Wei and follow his talking style, and I will act as Dimas. I will talk as Dimas, and then you reply once as Wei.
+
+{context}
+
+Dimas: {question}
+Wei:"""
+PROMPT = PromptTemplate(
+     template=prompt_template, input_variables=["context", "question"]
+)
+
+chain_type_kwargs = {"prompt": PROMPT}
+
+llm = ChatOpenAI(
+    model_name='gpt-3.5-turbo',
+    temperature=0.0,
+)
+
+chat_history = []
+qa = ConversationalRetrievalChain.from_llm(
+    llm,
+    vectorstore.as_retriever(search_kwargs={'k': 1}), 
+    chain_type = "stuff",
+    combine_docs_chain_kwargs= chain_type_kwargs,
+    verbose = True,
+    memory = conversational_memory)
+
+
+while True :
+     userInput = input('You (Dimas):')
+     result = qa({"question": userInput})
+     print(result["answer"])
+
+## Uncomment the section below to try ConversationChain
 custom_memory = ConversationBufferMemory(ai_prefix = "Kevin", human_prefix = "Mitch")
 
-
 ## I plan to fetch this from the DB in the future
-## Use COnversationChain, comment this chunk out when trying with ConversationalRetrievalChain below
+## Use ConversationChain, comment this chunk out when trying with ConversationalRetrievalChain below
 context = """
  Mitch is a second year Computer Engineering undergraduate who likes to play mobile games. He is from Indonesia and is the youngest son of his family. He came from a small city in North Sumatra called Medan. He has an older sister and is currently living alone in Singapore to pursue his studies.                                                                                                                                    """                                                                                                                                                                                                                                                                                                             
 # template = """The following is a friendly conversation between a Mitch and Kevin. You will act as Kevin and reply me in a Singaporean or heavy Singlish accent and I will act as Mitch. The history of conversation between Mitch and Kevin is provided below.""" + context + """
@@ -67,35 +106,5 @@ context = """
 
 
 
-### Using Conversational Chain to fetch information from context 
 
-prompt_template = """Use the following pieces of context to answer the question at the end. Analyze the given conversation in the context and understand the talking style as well as certain slang words that Wei often uses. You are to reply me as Wei and only Wei and follow his talking style, and I will act as Dimas. I will talk as Dimas, and then you reply once as Wei.
-
-{context}
-
-Dimas: {question}
-Wei:"""
-PROMPT = PromptTemplate(
-     template=prompt_template, input_variables=["context", "question"]
-)
-
-chain_type_kwargs = {"prompt": PROMPT}
-
-llm = ChatOpenAI(
-    model_name='gpt-3.5-turbo',
-    temperature=0.0,
-)
-
-
-qa = ConversationalRetrievalChain.from_llm(
-    llm,
-    vectorstore.as_retriever(), 
-    chain_type = "stuff",
-    combine_docs_chain_kwargs= chain_type_kwargs,
-    memory=conversational_memory)
-
-while True :
-     userInput = input('You (Dimas):')
-     result = qa({"question": userInput})
-     print(result["answer"])
 
