ab36e0b89a87223d24dfee1618dc0470b8b3190d
Mitch Malvin <e0773911@u.nus.edu>
2023-08-02 11:26:31 +0800

    add chat history storage

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 import openai
 import pinecone
+import boto3
 
 load_dotenv()
 app = Flask(__name__)
@@ -23,32 +24,66 @@ pinecone.init(
     environment=os.getenv('PINECONE_ENV')
 )
 
+s3_client = boto3.client('s3')
+session = boto3.Session()
+# s3_client.upload_file('./fabian_chat.txt', 'digital-immortality-chat-history', 'chat-history/history')
+
 embeddings = OpenAIEmbeddings()
 
-vectorstore = Pinecone.from_existing_index("test-index", embeddings)
+index_name = input('Please specify the Pinecone index to load the data from : ')
+if index_name not in pinecone.list_indexes():
+    print("Index does not exist, creating a new index called " + index_name)
+    pinecone.create_index(
+        name=index_name, 
+        metric="cosine",
+        dimension=1536)
 
-#open text file in read mode
-text_file = open("./chat_summary.txt", "r")
- 
-#read whole file to a string
-data = text_file.read()
- 
-#close file
-text_file.close()
- 
-response = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-        {"role": "system", "content": "Analyze the following conversation and return me the talking style, accent and common slang words that Fabian's wife likes to use." + data + "Return your answer in the following format: Talking style : ... ,Common Slang words : ... "},
-    ]
-)
+vectorstore = Pinecone.from_existing_index(index_name, embeddings)
 
-talkingStyleStr = response['choices'][0]['message']['content']
-print(talkingStyleStr)
+# #open text file in read mode
+# text_file = open("./chat_summary.txt", "r")
+ 
+# #read whole file to a string
+# data = text_file.read()
+ 
+# #close file
+# text_file.close()
+ 
+# response = openai.ChatCompletion.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#         {"role": "system", "content": "Analyze the following conversation and return me the talking style, accent and common slang words that Fabian's wife likes to use." + data + "Return your answer in the following format: Talking style : ... ,Common Slang words : ... "},
+#     ]
+# )
+
+# talkingStyleStr = response['choices'][0]['message']['content']
+# print(talkingStyleStr)
 
 
 #  create a memory object to track the inputs/outputs and hold a conversation
 conversational_memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+def retrieve_history():
+    response = s3_client.get_object(Bucket='digital-immortality-chat-history', Key='chat-history/history')
+
+    # The 'Body' attribute contains the content of the file
+    file_content = response['Body'].read().decode('utf-8')
+   
+    lines = file_content.splitlines()
+
+    for line in lines:
+        print('hmm')
+        line_without_prefix = line.replace('Human:', '').replace('AI:', '')
+
+        if line.startswith('Human:'):
+            conversational_memory.chat_memory.add_user_message(line_without_prefix)
+   
+        elif line.startswith('AI:'):
+             conversational_memory.chat_memory.add_ai_message(line_without_prefix)
+    print(conversational_memory.load_memory_variables({}))
+
+
+retrieve_history()
 
 userPrompt = input('Please enter the additional prompt that you would like to be added to the LLM : ')
 
@@ -58,7 +93,24 @@ userPrompt = input('Please enter the additional prompt that you would like to be
 
 prompt_template = """Use the following pieces of context to answer the question at the end.""" + userPrompt +  """
 
-This is the talking style of Fabian's wife :""" + talkingStyleStr + """
+This is the talking style of Fabian's wife : 
+
+Talking style: The wife's talking style can be described as casual and informal. She uses short and direct sentences, often omitting pronouns. She also makes frequent use of laughter and emojis to convey humor and express emotions.
+
+Accent: Based on the given conversation, it is not possible to determine the wife's accent.
+
+Common Slang words: Some common slang words used by the wife include:
+- lame: used to describe something boring or uninteresting
+- sia: a colloquial expression added at the end of a sentence for emphasis or surprise
+- alr: short form of "already"
+- wah: an exclamation used to express surprise or amazement
+- papa: refers to Fabian, possibly a pet name or term of endearment used by the wife
+- hahaha: laughter
+- bgt: abbreviation of "buy got" meaning buy something for someone
+- mama: refers to the wife, possibly a pet name or term of endearment used by Fabian
+- feebi: a pet name or nickname for Fabian
+- knn: a profanity abbreviation standing for "kao ni na" used to express anger or frustration
+
 
 This is the context given:
 {context}
@@ -95,6 +147,8 @@ def chat_bot():
     
     messages = data.get("message")
     result = qa({"question": messages})
+    print(conversational_memory.load_memory_variables({}))
+    
     return jsonify({"Status" : "success", "Message" : result["answer"]})
 
 
@@ -124,6 +178,20 @@ def ingest_data():
     os.remove('./ingestData/' + filename)
 
     return 'File successfully uploaded.', 200
+
+@app.route('/save',methods = ['POST'])
+def save_history() :
+    with open('./historyData/history.txt', "w") as file:
+        chat_history=conversational_memory.load_memory_variables({})['chat_history']
+        for message in chat_history:
+            if hasattr(message, 'content'):
+                if 'Human' in str(message.__class__):
+                    file.write(str('Human:' + message.content))
+                elif 'AI' in str(message.__class__):
+                    file.write(str('AI:' + message.content))
+
+    s3_client.upload_file('./historyData/history.txt', 'digital-immortality-chat-history', 'chat-history/history')    
+    return 'Success',200
 
 if __name__ == '__main__' : 
     app.run()
